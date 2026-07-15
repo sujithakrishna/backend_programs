05b5eaf6c53bb5882f772d6bcdcc438df77de0dd
sujithakrishna <122555597+sujithakrishna@users.noreply.github.com>
2026-07-15 23:20:09 +0530

    Add files via upload

---
+++ Area_of_rectangle.java
@@ -0,0 +1,13 @@
+package simple_programs;
+
+public class Area_of_rectangle {
+	public static void main(String[] args)
+	{
+		int l=20;
+		int b =40;
+		System.out.println("The area of rectangle is " +(l*b));
+		
+	}
+	
+
+}
