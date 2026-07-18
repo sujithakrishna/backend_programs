aef378b6a755668ff0fdbe3dfccc290feb9f3699
sujithakrishna <122555597+sujithakrishna@users.noreply.github.com>
2026-07-18 16:28:14 +0530

    Update Larges.java

--- chap_3/else if ladder/Larges.java
+++ chap_3/else if ladder/Larges.java
@@ -1,5 +1,36 @@
 package cha3_elseifladder;
 
+import java.util.Scanner;
+
 public class Larges {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter the first number: ");
+		int a =sc.nextInt();
+		System.out.print("Enter the second number: ");
+		int b =sc.nextInt();
+		System.out.print("Enter the third number: ");
+		int c =sc.nextInt();
+		
+		if(a>b && a>c)
+		{
+			System.out.println("The greatest among three numbers is "+ a);
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("The greatest among three numbers is "+ b);
+		}
+		else if(c>a && c>b)
+		{
+			System.out.println("The greatest among three numbers is "+ c);
+		}
+		else
+		{
+			System.out.println("Invalid input.");
+		}
+		
+	}
 
 }
